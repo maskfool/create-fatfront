b01522db632357a7246141ca21a87ad813a26662
Shubham <shubham@Shubhams-MacBook-Air.local>
2025-04-09 20:39:26 +0530

    added landing page

--- template/src/main.jsx
+++ template/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import '@fontsource/press-start-2p';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
